600a412e02e49c0926fcd0cc0ad191c8ff070672
Navneet <navneetmui@gmail.com>
2023-11-14 03:41:35 +0530

    Inital commit

---
+++ src/main/java/com/example/MailAuditPro/MailAuditProApplication.java
@@ -0,0 +1,13 @@
+package com.example.MailAuditPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAuditProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAuditProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MailAuditPro/MailAuditProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MailAuditPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAuditProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
