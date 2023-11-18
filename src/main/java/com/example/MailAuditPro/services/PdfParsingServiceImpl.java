5d70d30cdacd83b6aa82cdb70e34e7b10a101982
Juv3nil3 <navneetmui@gmail.com>
2023-11-18 13:16:53 +0530

    Add a method in the PdfParsingServiceImpl to extract text and images from PDF attachments, saving the extracted content to the PdfContent model and storing it in the repository.

--- src/main/java/com/example/MailAuditPro/services/PdfParsingServiceImpl.java
+++ src/main/java/com/example/MailAuditPro/services/PdfParsingServiceImpl.java
@@ -37,7 +37,61 @@ public class PdfParsingServiceImpl implements PdfParserService {
     public PdfContent extractPdfContentFromAttachment(byte[] pdfBytes, String messageId, String attachmentId)
             throws PdfParsingException {
 
-        return null;
+        PdfContent pdfContent = new PdfContent();
+        try (PDDocument document = PDDocument.load(pdfBytes)) {
+            // Extract text from the PDF
+            PDFTextStripper textStripper = new PDFTextStripper();
+            String pdfText = textStripper.getText(document);
+            pdfContent.setTextContent(pdfText);
+
+            // Extract images from the PDF
+            PDPageTree pages = document.getPages();
+            int imageIndex = 1;
+
+            for (PDPage page : pages) {
+                PDResources resources = page.getResources();
+                Iterable<COSName> xObjectNames = resources.getXObjectNames();
+
+                for (COSName xObjectName : xObjectNames) {
+                    if (resources.isImageXObject(xObjectName)) {
+                        PDImageXObject image = (PDImageXObject) resources.getXObject(xObjectName);
+
+                        // Convert the image to a buffered image
+                        BufferedImage bufferedImage = image.getImage();
+
+                        // Convert the buffered image to a byte array
+                        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                            ImageIO.write(bufferedImage, "PNG", baos);
+                            byte[] imageContent = baos.toByteArray();
+
+                            // Save the PdfImage entity to the repository
+                            PdfImage pdfImage = new PdfImage();
+                            pdfImage.setMessageId(messageId);
+                            pdfImage.setAttachmentId(attachmentId);
+                            pdfImage.setImageIndex(imageIndex);
+                            pdfImage.setImageContent(imageContent);
+
+                            // Add PdfImage to PdfContent
+                            pdfContent.addPdfImage(pdfImage);
+
+                            imageIndex++;
+                        } catch (IOException e) {
+                            // Handle exception when writing image to byte array
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            String errorMessage = String.format("Error during PDF processing for message ID: %s, attachment ID: %s",
+                    messageId, attachmentId);
+            throw new PdfParsingException(errorMessage, e);
+        }
+
+        // Save the PdfContent to the repository (consider moving this outside the function)
+        pdfRepository.save(pdfContent);
+
+        return pdfContent;
     }
 
 
